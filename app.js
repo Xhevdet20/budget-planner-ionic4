6ab598c094038d50d859abc7b75360c20b3ba8f0
Xhevdet <xhevdet1996@gmail.com>
2020-02-24 16:29:05 +0100

    second-2 commit

--- app.js
+++ app.js
@@ -2,18 +2,38 @@ const reasonInput = document.querySelector('#input-reason');
 const amountInput = document.querySelector('#input-amount');
 const cancelBtn = document.querySelector('#btn-cancel');
 const confirmBtn = document.querySelector('#btn-confirm');
+const expensesList = document.querySelector('#expenses-list');
+const totalExpensesOutput = document.querySelector('#total-expenses');
+let alertCtrl = document.querySelector('ion-alert-controller');
+
+
+let totalExpenses = 0;
+
+const clear = () => {
+    reasonInput.value = '';
+    amountInput.value = '';
+
+}
 
 confirmBtn.addEventListener('click', () => {
     const enteredReason = reasonInput.value;
     const enteredAmount = amountInput.value;
 
-    if(enteredReason.trim().length <= 0 || enteredAmount <= 0 || enteredAmount.trim().length <= 0){
-        console.log('not valid');
-        
-        return;
+    if(
+        enteredReason.trim().length <= 0 || 
+        enteredAmount <= 0 || 
+        enteredAmount.trim().length <= 0
+        ){
+           alert('Invalid values!');
+            return;
     }
+    const newItem = document.createElement('ion-item');
+    newItem.textContent = enteredReason + ": $" + enteredAmount;
 
-    console.log(enteredReason, enteredAmount);
-    
-    
+    expensesList.appendChild(newItem);
+    totalExpenses += +enteredAmount;
+    totalExpensesOutput.textContent = totalExpenses;
+    clear();
 });
+
+cancelBtn.addEventListener('click', clear);
